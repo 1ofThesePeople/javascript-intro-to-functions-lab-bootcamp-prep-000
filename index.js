2e6b6c7c467d38959d1795e97b40f8ccdfcc8ba7
Jeremy Che <jeremy.che48@gmail.com>
2018-01-25 23:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  var test = "I love you, Grandma.",
+    GmaTalk = "YES INDEED!",
+    GmaTalk2 = "I can\'t hear you!",
+    GmaTalk3 = "I love you, too.";
+  if (string === test) {
+    return GmaTalk
+  }
+  else if () {
+
+  }
+}
